fbfc851c063397499bc4199260aef06007f79ee1
Divy Arpit <arpitdivy@gmail.com>
2022-05-26 13:34:08 +0530

    init commit

---
+++ src/main/java/com/example/bot/controller/brain.java
@@ -0,0 +1,12 @@
+package com.example.bot.controller;
+
+public class brain {
+    apiresp resp;
+    public apiresp getData(){
+        resp=new apiresp();
+        resp.setData("This is dummy data");
+        resp.setExists("true");
+        resp.setStatuscode("200");
+        return resp;
+    }
+}
